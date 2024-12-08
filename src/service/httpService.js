a2ace6652f7e753dc5dea94534d6dcf81d2cced9
Vagish <Vagishmlk@gmail.com>
2024-12-08 12:40:24 +0530

    local host address, updated request structure

--- src/service/httpService.js
+++ src/service/httpService.js
@@ -1,28 +1,25 @@
 import axios from "axios";
+axios.defaults.withCredentials = true; // Global setting
 
 class HttpService {
+    static baseURL = "http://127.0.0.1:3000/api/v1";
 
-    static baseURL = "http://localhost:3000/api/v1";
-
-    static async get({
-        path,
-        query = "",
-        headers,
-    }) {
-        const response = await axios.get(`${HttpService.baseURL}/${path}?${query}`, { headers });
+    static async get({ path, query = "", headers }) {
+        const response = await axios.get(
+            `${HttpService.baseURL}/${path}?${query}`,
+            { headers }
+        );
         return response;
     }
 
-    static async post({
-        path,
-        body,
-        query,
-        headers,
-    }) {
-        const response = await axios.post(`${HttpService.baseURL}/${path}?${query}`, body, { headers });
+    static async post({ path, body, query = "", headers }) {
+        const response = await axios.post(
+            `${HttpService.baseURL}/${path}?${query}`,
+            JSON.stringify(body),
+            { headers }
+        );
         return response;
     }
-
 }
 
 export default HttpService;

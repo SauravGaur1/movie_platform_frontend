9f9b5e85dc5c2bbf40f0ef8dfa561d9fe2b64018
Vagish <Vagishmlk@gmail.com>
2024-12-08 12:49:43 +0530

    called component with category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import MovieCardModel from '../models/movieCardModel'
-import MovieCategorySection from '../layouts/movieCategorySection'
+import React from "react";
+import MovieCardModel from "../models/movieCardModel";
+import MovieCategorySection from "../layouts/movieCategorySection";
 
 function Home() {
     return (
         <div>
-            <MovieCategorySection></MovieCategorySection>
-            <MovieCategorySection></MovieCategorySection>
-            <MovieCategorySection></MovieCategorySection>
+            <MovieCategorySection
+                category={"Now playing"}
+            ></MovieCategorySection>
+            <MovieCategorySection
+                category={"Coming Soon"}
+            ></MovieCategorySection>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;

81d8ae616ed50e74e272d573d4aae520ad03a634
Vagish <Vagishmlk@gmail.com>
2024-12-08 12:44:40 +0530

    app content to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import { Link, NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser } from "../store/user/user-controller";
+import { setUserData } from "../store/user/user-slice";
+const Header = () => {
+    const dispatch = useDispatch();
+
+    const user = useSelector((store) => {
+        return store.user;
+    });
+    return (
+        <>
+            <header className="h-12 text-stone-950 flex w-300">
+                <nav className="ml-20 w-full space-x-40 mt-2.5">
+                    <Link to="/home">Home</Link>
+                    <NavLink to="/about">About</NavLink>
+                </nav>
+            </header>
+            <div className="bg-[var(--primary-color)] w-100 h-100 flex-col align-middle justify-center">
+                <div>
+                    <p>
+                        {user.username} : {user.age}
+                    </p>
+                </div>
+                <h1 className="text-3xl  font-bold underline">Hello world!</h1>
+                <div className="card flex justify-between">
+                    <button
+                        onClick={() =>
+                            dispatch(
+                                setUserData({
+                                    username: "Kanishka",
+                                    age: 6,
+                                })
+                            )
+                        }
+                    >
+                        Change name {user.username}
+                    </button>
+
+                    <button onClick={getUser}>getUser check in console</button>
+                    {/* <button onClick={getUser}>Login </button> */}
+                    <Link to="/signup">Signup</Link>
+                </div>
+            </div>
+        </>
+    );
+};
+export default Header;

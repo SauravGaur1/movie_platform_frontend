7ea6d950d1173b33647d3062e849c3b25765d231
Vagish <Vagishmlk@gmail.com>
2024-12-08 12:44:01 +0530

    fall back of failed routes to home

--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,26 @@
-import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
-import Home from './pages/Home';
-import About from './pages/About';
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import App from "./App";
+import Login from "./pages/Login";
+import Signup from "./pages/signup";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route path="home" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Route>,
-  ),
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="home" element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+
+            <Route path="*" element={<Navigate to="/home" />} />
+        </Route>
+    )
 );
 
-export default router
+export default router;

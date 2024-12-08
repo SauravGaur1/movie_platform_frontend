4a4fca196c917bb2475c73e0921a33c6fc973359
Vagish <Vagishmlk@gmail.com>
2024-12-08 12:41:55 +0530

    moved user to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
-import { useDispatch, useSelector } from 'react-redux'
-import { setUserData } from './store/user/user-slice'
-import { getUser } from './store/user/user-controller';
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "./store/user/user-slice";
+import { getUser } from "./store/user/user-controller";
+import { Link, NavLink, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-
-  const dispatch = useDispatch();
-
-  const user = useSelector((store) => {
-    return store.user;
-  })
-  
-
-  return (
-    <>
-      <header className="h-12 text-stone-950 flex w-300">
-        <nav className="ml-20 w-full space-x-40 mt-2.5">
-          <Link to="/home">Home</Link>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
-      <div className="bg-[var(--primary-color)] w-100 h-100 flex-col align-middle justify-center">
-        <div>
-          <p>
-            {' '}
-            {user.username} : {user.age}{' '}
-          </p>
-        </div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        <div className="card">
-          <button
-            onClick={() =>
-              dispatch(
-                setUserData({
-                  username: 'Kanishka',
-                  age: 6,
-                }),
-              )
-            }
-          >
-            Change name {user.username}
-          </button>
-
-          <button onClick={getUser}>getUser check in console</button>
-        </div>
-      </div>
-      <div className='bg-black text-slate-50'>
-        <Outlet />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <div className="bg-black text-slate-50">
+                <Outlet />
+            </div>
+            <Footer />
+        </>
+    );
 }
 
-export default App
+export default App;
